cc1229254db155f639f70b449fdf85b124cf974d
zzzyoonnn <dwsrh03@gmail.com>
2026-03-12 18:49:35 +0900

    feat: LottoGenerator 로또 번호 생성 기능 구현

--- src/main/java/lotto/domain/LottoGenerator.java
+++ src/main/java/lotto/domain/LottoGenerator.java
@@ -1,5 +1,9 @@
 package lotto.domain;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.List;
+import lotto.Lotto;
 
 public class LottoGenerator {
 
@@ -12,8 +16,28 @@ public class LottoGenerator {
 
   public void run() {
     System.out.println(money + "원을 입력 받았습니다.");
-    System.out.println(money / LOTTO_PRICE + "장의 로또를 구매합니다.");
-    // 로또 번호 생성 로직
+    int count = money / LOTTO_PRICE;
+    System.out.println(count + "장의 로또를 구매합니다.");
+
+    List<Lotto> lottos = generateLottos(count);
+    System.out.println(lottos.size() + "개를 구매했습니다.");
+    for (Lotto lotto : lottos) {
+      System.out.println(lotto.getNumbers());
+    }
   }
+
+  private List<Lotto> generateLottos(int count) {
+    List<Lotto> lottos = new ArrayList<>();
+    for (int i = 0; i < count; i++) {
+      lottos.add(generateLotto());
+    }
+    return lottos;
+  }
+
+  private Lotto generateLotto() {
+    List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+    return new Lotto(numbers);
+  }
+
 
 }
